9f39a503fd0b11c9a36289b2d31fcb9a37820bcd
kkipngenokoech <kkipngenokoech22@gmail.com>
2022-07-21 19:02:02 +0300

    added the entity logic of the game

---
+++ README.md
@@ -0,0 +1,1 @@
+A rock paper scissors game
--- rockpaperscissors.js
+++ rockpaperscissors.js
@@ -1,9 +1,65 @@
-function GetComputerChoice(list)
+function getcomputerchoice(list)
 {
     const randomvalue = Math.floor(Math.random()*list.length);
     const randomitem = list[randomvalue];
     return randomitem;
 }
 const bucketlist = ["rock","paper","scissors"]
-const computerchoice = GetComputerChoice(bucketlist);
-console.log(computerchoice);
+const computerchoice = getcomputerchoice(bucketlist);
+//console.log(computerchoice);
+function playround (computerselection, playerselection)
+{
+    if (computerchoice === "rock")
+    {
+        if (playerselection === "scissors")
+        {
+            return `you lose! ${computerchoice} beats ${playerselection}`;
+        }
+        else if (playerselection === "paper")
+        {
+            return `you win!yay, ${playerselection} beats ${computerchoice}`;
+        }
+        else
+        {
+              return `you both chose ${computerchoice}, its a draw`;
+        }
+    }
+    else if (computerchoice === "paper")
+    {
+        if (playerselection === "rock")
+        {
+            return `you lose! ${computerchoice} beats ${playerselection}`;
+        }
+        else if (playerselection === "scissors")
+        {
+            return `you win ${playerselection} beats ${computerchoice}`;
+        }
+        else
+        {
+              return `you both chose ${computerchoice}, its a draw`;
+        }
+    }
+    else
+    {
+        if (playerselection === "rock")
+        {
+            return `you win!, ${playerselection} beats ${computerchoice}`;
+        }
+        else if (playerselection === "paper")
+        {
+            return `you lose!, ${computerchoice} beats ${playerselection}`;
+        }
+        else
+        {
+            return `let's call it a draw, ${playerselection} equals ${computerchoice}`;
+        }
+    }
+}
+const playerinput = "rock";//prompt("enter your choice:",'')
+const results = playround(computerchoice,playerinput);
+console.log(results);
+// function returnresults(results)
+// {
+//     return results;
+// }
+// returnresults(results);
